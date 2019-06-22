99d674514d494d6008612b45e9dd7c9779d783dc
Lassi Laiho <lassi.laiho0@gmail.com>
2019-06-22 16:10:49 +0300

    Move recipe card to a new widget

---
+++ lib/recipe_card.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'recipe.dart';
+import 'recipe_deletion_snack_bar.dart';
+import 'recipe_details.dart';
+import 'recipe_editor.dart';
+
+class RecipeCard extends StatelessWidget {
+  final Recipe recipe;
+
+  const RecipeCard({Key key, this.recipe}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final recipeStore = Provider.of<RecipeStore>(context);
+    return Card(
+      child: ListTile(
+        title: Text(recipe.name.isEmpty ? 'Unnamed recipe' : recipe.name),
+        trailing: PopupMenuButton<int>(
+          icon: const Icon(Icons.more_vert),
+          itemBuilder: (context) => const [
+                PopupMenuItem(
+                  value: 0,
+                  child: Text('Edit'),
+                ),
+                PopupMenuItem(
+                  value: 1,
+                  child: Text('Remove'),
+                )
+              ],
+          onSelected: (choice) {
+            switch (choice) {
+              case 0:
+                Navigator.push<void>(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RecipeEditor(
+                          initialRecipe: recipe,
+                          onEditFinished: recipeStore.updateRecipe,
+                        ),
+                  ),
+                );
+                break;
+              case 1:
+                showRecipeDeletionSnackBar(
+                  context,
+                  recipeStore.removeRecipeWithDelay(
+                    recipe.id,
+                    recipeDeletionUndoDuration,
+                  ),
+                  recipeDeletionUndoDuration,
+                );
+                break;
+            }
+          },
+        ),
+        onTap: () async {
+          final result = await Navigator.push<RecipeDeletionAction>(
+            context,
+            MaterialPageRoute(
+              builder: (context) => RecipeDetails(recipe.id),
+            ),
+          );
+          if (result != null) {
+            showRecipeDeletionSnackBar(
+              context,
+              result.operation,
+              result.duration,
+            );
+          }
+        },
+      ),
+    );
+  }
+}
--- lib/recipe_view.dart
+++ lib/recipe_view.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'recipe.dart';
-import 'recipe_deletion_snack_bar.dart';
-import 'recipe_details.dart';
+import 'recipe_card.dart';
 import 'recipe_editor.dart';
 
 class RecipeView extends StatefulWidget {
@@ -14,7 +13,6 @@ class RecipeView extends StatefulWidget {
 class _RecipeViewState extends State<RecipeView> {
   @override
   Widget build(BuildContext context) {
-    final addRecipe = Provider.of<RecipeStore>(context).addRecipe;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Recipes'),
@@ -23,68 +21,9 @@ class _RecipeViewState extends State<RecipeView> {
         builder: (context, recipeStore, child) => ListView(
               padding: const EdgeInsets.all(8),
               children: recipeStore.recipes
-                  .map((recipe) => Card(
+                  .map((recipe) => RecipeCard(
                         key: Key(recipe.id.toString()),
-                        child: ListTile(
-                          title: Text(recipe.name.isEmpty
-                              ? 'Unnamed recipe'
-                              : recipe.name),
-                          trailing: PopupMenuButton<int>(
-                            icon: const Icon(Icons.more_vert),
-                            itemBuilder: (context) => const [
-                                  PopupMenuItem(
-                                    value: 0,
-                                    child: Text('Edit'),
-                                  ),
-                                  PopupMenuItem(
-                                    value: 1,
-                                    child: Text('Remove'),
-                                  )
-                                ],
-                            onSelected: (choice) {
-                              switch (choice) {
-                                case 0:
-                                  Navigator.push<RecipeDetails>(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => RecipeEditor(
-                                            initialRecipe: recipe,
-                                            onEditFinished:
-                                                recipeStore.updateRecipe,
-                                          ),
-                                    ),
-                                  );
-                                  break;
-                                case 1:
-                                  showRecipeDeletionSnackBar(
-                                    context,
-                                    recipeStore.removeRecipeWithDelay(
-                                      recipe.id,
-                                      recipeDeletionUndoDuration,
-                                    ),
-                                    recipeDeletionUndoDuration,
-                                  );
-                                  break;
-                              }
-                            },
-                          ),
-                          onTap: () async {
-                            final result =
-                                await Navigator.push<RecipeDeletionAction>(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => RecipeDetails(recipe.id),
-                              ),
-                            );
-                            if (result != null) {
-                              showRecipeDeletionSnackBar(
-                                context,
-                                result.operation,
-                                result.duration,
-                              );
-                            }
-                          },
-                        ),
+                        recipe: recipe,
                       ))
                   .toList(),
             ),
@@ -93,12 +32,12 @@ class _RecipeViewState extends State<RecipeView> {
         child: const Icon(Icons.add),
         backgroundColor: Theme.of(context).accentColor,
         onPressed: () {
-          Navigator.push<RecipeEditor>(
+          Navigator.push<void>(
             context,
             MaterialPageRoute(
               builder: (context) => RecipeEditor(
                     initialRecipe: null,
-                    onEditFinished: addRecipe,
+                    onEditFinished: Provider.of<RecipeStore>(context).addRecipe,
                     title: 'Add Recipe',
                   ),
             ),
